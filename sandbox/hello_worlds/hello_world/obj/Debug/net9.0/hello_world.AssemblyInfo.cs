8b411f94cb272b9bcd2c331e08627a17250ea0ca
Holly Vose <holly.vose@cuanschutz.edu>
2025-10-23 14:45:21 -0600

    checking this works

--- sandbox/hello_worlds/hello_world/obj/Debug/net9.0/hello_world.AssemblyInfo.cs
+++ sandbox/hello_worlds/hello_world/obj/Debug/net9.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003bae6365b3a2eddac979995ef8650f6c6da313")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
