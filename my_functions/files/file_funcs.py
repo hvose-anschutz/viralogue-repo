6d075fcdfedf9917b0205d007878606e6e71498a
Holly Vose <holly.vose@cuanschutz.edu>
2025-10-31 11:58:24 -0600

    pylint fixes to my_functions/files

--- my_functions/files/file_funcs.py
+++ my_functions/files/file_funcs.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
-import re
+"""All reused functions across the viralogue-repo."""
 
-def tab_separator(my_file: str, delimiter: str="\t",header: bool=False) -> list[str]:
-    """Separates a delimited file into a list object. The delimiter can be specified, but tab is the default."""
+def tab_separator(my_file: str, 
+                  delimiter: str="\t",
+                  header: bool=False) -> list[str]:
+    """Separates a delimited file into a list object. The delimiter can be specified, 
+    but tab is the default."""
     return_list = []
-    with open(my_file,"r") as f:
+    with open(my_file,"r",encoding="utf-8") as f:
         for idx, line in enumerate(f.readlines()):
-            if (header == True) and (idx == 0):
+            if (header) and (idx == 0):
                 pass
             else:
                 return_list.append(line.strip().split(delimiter))
     f.close()
     return return_list
 
-def write_dict_to_file(my_filename: str, my_dict: dict, delimiter: str="\t", keep_flags: bool=False):
+def write_dict_to_file(my_filename: str, my_dict: dict, delimiter: str="\t", 
+                       keep_flags: bool=False):
     """Outputs the contents of a dictionary to a specified file.\n
-    my_filename: the desired filename (if no path specified, it will output to the current directory)\n
-    my_dict: the dictionary to print out\n
+    my_filename: the desired filename (if no path specified, it will output to the 
+    current directory)\n my_dict: the dictionary to print out\n
     delimiter (default tab): the delimiter to space between keys and values\n
     keep_flags (default False): adds 'key' and 'value' to each column for troubleshooting."""
     with open(my_filename, "w") as f:
-        if keep_flags == False:
+        if not keep_flags:
             for key,value in my_dict.items():
                 f.write(key + delimiter + str(value) + "\n")
         else:
             for key,value in my_dict.items():
                 print("key: " + key + delimiter + "value" + str(value) + "\n")
-    
-    return None
+                
+                           
