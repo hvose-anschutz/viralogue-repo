50152744a08414087450eb7e7330446c6b0c01b2
Holly Vose <holly.vose@cuanschutz.edu>
2025-11-03 13:02:51 -0700

    removed trailing whitespace in sj_analyzer.py

--- my_scripts/sj_analyzer.py
+++ my_scripts/sj_analyzer.py
@@ -9,7 +9,7 @@ ALL_SJ = {}
 for item in my_sj:
     new_title = item[0] + ":" + item[1] + "-" + item[2]
     total = int(item[7]) + int(item[8])
-    new_accession = "test" + ":" + str(total) + "=u" + item[7] + "+m" + item[8] 
+    new_accession = "test" + ":" + str(total) + "=u" + item[7] + "+m" + item[8]
 
     if new_title not in ALL_SJ:
         ALL_SJ[new_title] = [new_accession]
