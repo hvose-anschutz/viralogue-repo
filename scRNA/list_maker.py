d7809c11534877db6d1b3152164627732e3dd89b
Holly Vose <holly.vose@cuanschutz.edu>
2025-11-03 10:41:45 -0700

    fixing pylint issues with list_maker.py
    changed constants to be UPPER_CASE
    fixed missing encoding
    removed use of 'id' parameter
    created docstring for write_to_file()

--- scRNA/list_maker.py
+++ scRNA/list_maker.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
 
-"""Creates a list from a tab-delimited file given an index to create from."""
+"""Creates a list from a tab-delimited file given an index to create from. Must be run with Python 3.10+"""
 
 import re
 
 def write_to_file(filename,list_to_write):
-    with open(filename,"w") as g:
-        for id in list_to_write:
-            g.write(id + "\n")
+    """Writes a provided list into a new file"""
+    with open(filename,"w",encoding="utf=8") as g:
+        for my_id in list_to_write:
+            g.write(my_id + "\n")
     g.close()
 
-my_reads_L1 = []
-my_reads_L2 = []
-my_reads_L3 = []
+MY_READS_L1 = []
+MY_READS_L2 = []
+MY_READS_L3 = []
 
 with open("mhvy_infected_blastn_3.out","r") as f:
     for match in f.readlines():
         match_info = match.strip().split()
-        lane = re.search("T3:(\d):",match_info[1])
+        lane = re.search(r"T3:(\d):",match_info[1])
         match int(lane.group(1)):
             case 1:
-                my_reads_L1.append(match_info[1])
+                MY_READS_L1.append(match_info[1])
             case 2:
-                my_reads_L2.append(match_info[1])
+                MY_READS_L2.append(match_info[1])
             case 3:
-                my_reads_L3.append(match_info[1])
+                MY_READS_L3.append(match_info[1])
 f.close()
 
-write_to_file("mhvy_sample3_L001.txt",my_reads_L1)
-write_to_file("mhvy_sample3_L002.txt",my_reads_L2)
-write_to_file("mhvy_sample3_L003.txt",my_reads_L3)
+write_to_file("mhvy_sample3_L001.txt",MY_READS_L1)
+write_to_file("mhvy_sample3_L002.txt",MY_READS_L2)
+write_to_file("mhvy_sample3_L003.txt",MY_READS_L3)
