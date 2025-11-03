19711605ca1f119bdc334c3a0b414bfff374938a
Holly Vose <holly.vose@cuanschutz.edu>
2025-11-03 11:28:30 -0700

    fixing trailing whitespaces/newlines, added encoding to functions

--- my_functions/files/file_funcs.py
+++ my_functions/files/file_funcs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """All reused functions across the viralogue-repo."""
 
-def tab_separator(my_file: str, 
+def tab_separator(my_file: str,
                   delimiter: str="\t",
                   header: bool=False) -> list[str]:
     """Separates a delimited file into a list object. The delimiter can be specified, 
@@ -16,21 +16,18 @@ def tab_separator(my_file: str,
     f.close()
     return return_list
 
-def write_dict_to_file(my_filename: str, my_dict: dict, 
-                       delimiter: str="\t", 
+def write_dict_to_file(my_filename: str, my_dict: dict,
+                       delimiter: str="\t",
                        keep_flags: bool=False):
     """Outputs the contents of a dictionary to a specified file.\n
     my_filename: the desired filename (if no path specified, it will output to the 
     current directory)\n my_dict: the dictionary to print out\n
     delimiter (default tab): the delimiter to space between keys and values\n
     keep_flags (default False): adds 'key' and 'value' to each column for troubleshooting."""
-    with open(my_filename, "w") as f:
+    with open(my_filename, "w",encoding="utf-8") as f:
         if not keep_flags:
             for key,value in my_dict.items():
                 f.write(key + delimiter + str(value) + "\n")
         else:
             for key,value in my_dict.items():
                 print("key: " + key + delimiter + "value: " + str(value) + "\n")
-                
-                           
-
