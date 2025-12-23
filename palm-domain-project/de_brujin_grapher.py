6055cd20883393744a9b9ca88aa9e6f55ff36503
Holly Vose <holly.vose@cuanschutz.edu>
2025-12-23 10:30:50 -0700

    adding structure to anchor system, also updating sandbox because i forgot how lists work

--- palm-domain-project/de_brujin_grapher.py
+++ palm-domain-project/de_brujin_grapher.py
@@ -2,12 +2,14 @@
 
 """Takes in a list of parsed fastas and determines the most conserved region."""
 
+import re
+
 #This is the initial version
 
-my_reads = ["batty","batter","ratty","ratter"]
+my_reads = ["ratty","batter","batty","batter","abattey"]
 
 #0 is the start, 1 is the end
-GRAPH = {0:[]}
+GRAPH = {}
 K = 3
 
 #generating the graph by looking for an overlap of k-1
@@ -16,19 +18,17 @@ for read in my_reads:
     for i in range(READ_LENGTH-K+1):
         k_mer = read[i:i+K]
         #print(f"processing kmer {k_mer} with a range of {i} to {i+K}")
-        if i == 0:
-            GRAPH[0].append(k_mer)
         if k_mer not in GRAPH:
             if i + K == READ_LENGTH:
-                GRAPH[k_mer] = [1]
+                GRAPH[k_mer] = []
             else:
                 next_k_mer = read[i+1:i+K+1]
                 GRAPH[k_mer] = [next_k_mer]
         else:
-            if i + K == READ_LENGTH:
-                GRAPH[k_mer].append(1)
-            else:
+            if i + K != READ_LENGTH:
                 next_k_mer = read[i+1:i+K+1]
+                #need to check for duplicate anchors
+                
                 GRAPH[k_mer].append(next_k_mer)
 
 print(GRAPH)
@@ -36,8 +36,32 @@ print(GRAPH)
 #processing the graph to find the most conserved path
 
 stitching = True
-next_check = 0
-prev_high = 0
-most_nodes = 0
+highest_count = 0
+start_point = ""
 conserved = ""
+anchor = 0
 
+for keys, values in GRAPH.items():
+    #here, we pick a starting point that meets the criteria of (len(values) = len(my_reads)) && (len(set(values)) = 1)
+        if len(values) > highest_count:
+            start_point = keys
+            highest_count = len(values)
+
+conserved = start_point[0:1]
+
+while stitching:
+    #assign the next key in the graph (index doesn't matter because they all point to the same node)
+    next_key = GRAPH[start_point][0]
+
+    #check to see if the next node follows the rules
+    if len(set(GRAPH[next_key])) > 1:
+        #we assume no errors, and therefore this is the end of the stitch
+        conserved = conserved + next_key
+        stitching = False
+    else:
+        #the stitching continues, but we need to check for a duplicate node
+
+        conserved = conserved + next_key[0:1]
+        start_point = next_key
+
+print(conserved)
---
+++ sandbox/sandbox.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Sandbox for testing various things."""
+
+mix_list = [1,[2],[3]]
+
+for item in mix_list:
+    print(item)
