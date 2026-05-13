05aaa9f608557d1ea28ea3c9b0ee725360320f52
Holly Vose <holly.vose@cuanschutz.edu>
2026-05-13 14:51:32 -0600

    fixing linting issues in pol_puller.py

--- my_scripts/pol_puller.py
+++ my_scripts/pol_puller.py
@@ -28,7 +28,7 @@ with open("Mmus38.txt","r",encoding="utf-8") as g:
         #print("checking line " + line)
         my_items = line.strip().split("\t")
         #print("checking " + my_items[10])
-        valid_pol = re.search(f"[P|p]ol",my_items[10])
+        valid_pol = re.search("[P|p]ol",my_items[10])
         if valid_pol is not None:
             #print("found a pol")
             #now we check for the valid ERVS
@@ -37,18 +37,27 @@ with open("Mmus38.txt","r",encoding="utf-8") as g:
                 valid_iap = re.search("IAP",my_items[14])
                 #print("checking " + my_items[14])
                 if valid_mmtv is not None:
-                    #we have found a valid MMTV ERV, can put into the list to search for 
-                    FILTERED_ERVS.append([my_items[0],"MMTV",my_items[5],MMUS_FA_DICT[">" + my_items[0]]])
+                    #we have found a valid MMTV ERV, can put into the list to search for
+                    FILTERED_ERVS.append([my_items[0],
+                                          "MMTV",
+                                          my_items[5],
+                                          MMUS_FA_DICT[">" + my_items[0]]])
                 elif valid_iap is not None:
                     #we have found a valid IAP
-                    FILTERED_ERVS.append([my_items[0],"IAP",my_items[5],MMUS_FA_DICT[">" + my_items[0]]])
+                    FILTERED_ERVS.append([my_items[0],
+                                          "IAP",
+                                          my_items[5],
+                                          MMUS_FA_DICT[">" + my_items[0]]])
                 else:
                     #now we get to check all of the col 15 ERVs in VALID_ERVS
                     for erv in VALID_ERVS:
                         check_erv = re.search(erv,my_items[15])
                         if check_erv is not None:
                             #we found a valid erv
-                            FILTERED_ERVS.append([my_items[0],erv,my_items[5],MMUS_FA_DICT[">" + my_items[0]]])
+                            FILTERED_ERVS.append([my_items[0],
+                                                  erv,
+                                                  my_items[5],
+                                                  MMUS_FA_DICT[">" + my_items[0]]])
                             break
 g.close()
 
