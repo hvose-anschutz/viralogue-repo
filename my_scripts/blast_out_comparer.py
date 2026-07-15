ed0ebb3d24469326fa881ac87b90336202fbf308
Holly Vose <holly.vose@cuanschutz.edu>
2026-07-15 11:56:28 -0600

    removing blast_comparer for redundancy, also testing venv git authentication

--- my_scripts/blast_out_comparer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""Interprets outfmt files from BLAST to compare regions"""
-
-import sys
-
-with open(sys.argv[1],"r",encoding="utf-8") as f:
-    for line in f.readlines():
-        my_line = line.strip().split()
